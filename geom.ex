44dfa6dcccd882ba80c0241d1859dc1839a73f2f
Kerry Buckley <kerryjbuckley@gmail.com>
2015-07-10 17:29:16 +0100

    3-4: Take args as a tuple

--- geom.ex
+++ geom.ex
@@ -9,17 +9,18 @@ defmodule Geom do
 
   ## Examples:
 
-      iex> Geom.area :rectangle, 2, 4
+      iex> Geom.area {:rectangle, 2, 4}
       8
 
-      iex> Geom.area :triangle, 2, 4
+      iex> Geom.area {:triangle, 2, 4}
       4.0
 
-      iex> Geom.area :ellipse, 2, 4
+      iex> Geom.area {:ellipse, 2, 4}
       25.132741228718345
   """
-  def area(:rectangle, length, width) when length > 0 and width > 0, do: length * width
-  def area(:triangle, base, height) when base > 0 and height > 0, do: 0.5 * base * height
-  def area(:ellipse, length, width) when length > 0 and width > 0, do: :math.pi * length * width
-  def area(_, _, _), do: 0
+  def area({shape, dimension_1, dimension_2}), do: area(shape, dimension_1, dimension_2)
+  defp area(:rectangle, length, width) when length > 0 and width > 0, do: length * width
+  defp area(:triangle, base, height) when base > 0 and height > 0, do: 0.5 * base * height
+  defp area(:ellipse, length, width) when length > 0 and width > 0, do: :math.pi * length * width
+  defp area(_, _, _), do: 0
 end

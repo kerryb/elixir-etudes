23df6a2657e05fa9915ac04853a0bf24947bd0af
Kerry Buckley <kerryjbuckley@gmail.com>
2015-07-10 15:06:59 +0100

    3-1: accept different shapes using pattern matching

--- geom.ex
+++ geom.ex
@@ -2,9 +2,23 @@ defmodule Geom do
   @moduledoc """
   Geometry stuff
   """
+
   @doc """
-  Calculate the area of a rectangle with the given length and width. If
-  arguments are omitted, they default to 1.
+  Calculate the area of a shape with the given dimensions. Supported shapes are
+  :rectangle, :triangle and :ellipse.
+
+  ## Examples:
+
+      iex> Geom.area :rectangle, 2, 4
+      8
+
+      iex> Geom.area :triangle, 2, 4
+      4.0
+
+      iex> Geom.area :ellipse, 2, 4
+      25.132741228718345
   """
-  def area(length \\ 1, width \\ 1), do: length * width
+  def area(:rectangle, length, width), do: length * width
+  def area(:triangle, base, height), do: 0.5 * base * height
+  def area(:ellipse, length, width), do: :math.pi * length * width
 end

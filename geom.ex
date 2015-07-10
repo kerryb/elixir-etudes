c39915707403d7fdcae9b4b23d009634e4f5f2ab
Kerry Buckley <kerryjbuckley@gmail.com>
2015-07-10 14:07:44 +0100

    2-3: add documentation

--- geom.ex
+++ geom.ex
@@ -1,3 +1,10 @@
 defmodule Geom do
+  @moduledoc """
+  Geometry stuff
+  """
+  @doc """
+  Calculate the area of a rectangle with the given length and width. If
+  arguments are omitted, they default to 1.
+  """
   def area(length \\ 1, width \\ 1), do: length * width
 end
